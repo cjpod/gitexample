6af5f42a749f80fb5d8c7c109c55dacdd7a848f5
cjpod <odriscoll195@hotmail.com>
2013-04-10 13:13:30 +0100

    initial commit

---
+++ gitexample/src/ie/gcd/griffith/sample.java
@@ -0,0 +1,17 @@
+package ie.gcd.griffith;
+
+public class sample {
+	
+	public sample(){
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
